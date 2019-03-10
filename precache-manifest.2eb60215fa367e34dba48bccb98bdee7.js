9198903650506a8cc3cd8e4665c4814cabf4f313
sharakOrest <sharak.orest@gmail.com>
2019-03-10 18:57:50 +0100

    HashRouter added and fontawesone to contact

---
+++ precache-manifest.2eb60215fa367e34dba48bccb98bdee7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ac5c137b85a9e5a2bfd",
+    "url": "/natalie-sharak.github.io/static/js/runtime~main.048a9cd0.js"
+  },
+  {
+    "revision": "700190c81da0ff869516",
+    "url": "/natalie-sharak.github.io/static/js/main.f1772313.chunk.js"
+  },
+  {
+    "revision": "229e90b12534dd53a9d6",
+    "url": "/natalie-sharak.github.io/static/js/2.5f3f7d45.chunk.js"
+  },
+  {
+    "revision": "700190c81da0ff869516",
+    "url": "/natalie-sharak.github.io/static/css/main.eb2b5dca.chunk.css"
+  },
+  {
+    "revision": "ad1c0ad569165a7b7d68538e2fa3806f",
+    "url": "/natalie-sharak.github.io/index.html"
+  }
+];
